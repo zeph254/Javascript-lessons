6a865595a7800ec212417d6c4d13d9c85dc80884
zeph254 <ularezephaniah@gmail.com>
2025-03-08 17:13:04 +0300

    Split Strings

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -5,3 +5,25 @@ console.log(num1 + num2);
 
 Math.round(1.6); // 2
 Math.round(1.4); // 1
+
+
+
+
+
+function solution(str){
+    let result = [];
+    
+    for (let i = 0; i < str.length; i += 2) {
+      let pair = str[i] + (str[i + 1] || "_" )
+      result.push(pair);
+    }
+    
+    return result;
+  }
+
+
+
+  console.log(solution("abc"));      // ['ab', 'c_']
+  console.log(solution("abcdef"));   // ['ab', 'cd', 'ef']
+  console.log(solution("a"));        // ['a_']
+  console.log(solution(""));     
