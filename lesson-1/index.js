133c8da6549345530eabcbd2173757c98ee68486
zeph254 <ularezephaniah@gmail.com>
2025-03-08 16:19:26 +0300

    lesson 2

---
+++ lesson-1/index.js
@@ -0,0 +1,7 @@
+console.log('Hello, world!');
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+
+Math.round(1.6); // 2
+Math.round(1.4); // 1
---
+++ lesson-2/index.js
@@ -0,0 +1,4 @@
+// alert("hello world");
+"play" + "ground"; // "playground"
+typeof 1; // "number"
+"$" + 1; // "$1"
