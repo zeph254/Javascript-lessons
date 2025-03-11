5f98f399f312f23f56ed461681895833713119ad
zeph254 <ularezephaniah@gmail.com>
2025-03-11 12:09:17 +0300

    variables

---
+++ lesson-3/dataTypes.js
@@ -0,0 +1,49 @@
+// Numbers
+let myAge = 25;
+let myHeight = 5.8;
+let myWeight = 160;
+let myShoeSize = 10;
+
+
+// boolean
+let isMarried = false;
+let isEmployed = true;
+let isHappy = true;
+
+// string
+let myName = "John Doe";
+let myAddress = "123 Main St, City, State, 12345";
+let myPhone = "123-456-7890";   
+
+
+// null
+let myCar = null;
+let myCat = null;
+let myDog = null;
+
+// undefined
+let myJob;
+let myHobby;
+console.log(myJob, myHobby);
+
+
+// Symbol
+let mySymbol = Symbol("mySymbol");
+console.log(mySymbol);
+
+// Objects
+let myObject = {
+    name: "John Doe",
+    age: 25,
+    address: "123 Main St, City, State, 12345",
+    phone: "123-456-7890"
+};
+console.log(myObject);
+
+// Arrays
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
+
+// Typeof
+let myVariable = 10;
+console.log(typeof myVariable); // number
---
+++ lesson-3/variables.js
@@ -0,0 +1,26 @@
+// var firstname = "John";
+// let lastname = "Doe";
+// const age = 30; // age is a constant variable
+
+let firstname;
+let lastname;
+let age;
+
+
+firstname = "John"; // firstname is a variable that can be reassigned
+lastname = "Doe"; // lastname is a variable that can be reassigned
+age = 30; // age is a variable that can be reassigned
+
+console.log(firstname, lastname, age);
+
+let message = "Hello, world!";// message is a variable that can be reassigned
+console.log(message);
+message = "Hello, JavaScript!";
+console.log(message);
+
+
+// const
+const PI = 3.14159;// cannot be reassigned - assings data to a variable that cannot be changed
+console.log(PI);
+
+// PI = 3.14; // TypeError: Assignment to constant variable.
